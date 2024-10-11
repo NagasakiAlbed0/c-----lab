8f3d0fdd8ff0830a3e8566700dfb7a7f60bd1e16
Davidenco Nicolae <nagasakialbedo@gmail.com>
2024-10-11 14:06:02 +0300

    Update Joc.h
    modificarea

--- Joc.h
+++ Joc.h
@@ -1,19 +1,26 @@
-#ifndef JOC_H
-#define JOC_H
-
-#include "Tabla.h"
-
-using namespace std;
-
 class Joc {
 public:
     Tabla tabla;
     char marcaj_curent;
     int jucator_curent;
 
-    Joc(char marcaj_jucator1);
+    // Constructori
+    Joc();  // Constructor implicit
+    Joc(char marcaj_jucator1);  // Constructor cu parametri
+    Joc(const Joc& other);  // Constructor de copiere
+
+    // Operator de copiere
+    Joc& operator=(const Joc& other);
+
+    // Operator de comparatie
+    bool operator==(const Joc& other) const;
+
+    // Operator << pentru afisare
+    friend ostream& operator<<(ostream& os, const Joc& joc);
+
+    // Operator >> pentru citire
+    friend istream& operator>>(istream& is, Joc& joc);
+
     void schimbaJucator();
     void incepeJoc();
 };
-
-#endif
