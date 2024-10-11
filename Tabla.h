ea99fabb79ceb60c6cd46986f6f7b5b463eddd14
Davidenco Nicolae <nagasakialbedo@gmail.com>
2024-10-11 14:08:03 +0300

    Tabla.h

--- Tabla.h
+++ Tabla.h
@@ -1,18 +1,18 @@
-#ifndef TABLA_H
-#define TABLA_H
-
-#include <iostream>
-
-using namespace std;
-
 class Tabla {
 public:
-    char tabla[3][3]; 
+    char tabla[3][3];
 
-    Tabla();
+    // Constructri
+    Tabla();  // Constructor implicit
+    Tabla(const Tabla& other);  // Constructor de copiere
+
+    // Operator de copier
+    Tabla& operator=(const Tabla& other);
+
+    // Operator de comparare
+    bool operator==(const Tabla& other) const;
+
     void deseneazaTabla();
     bool puneMarcaj(int pozitie, char marcaj);
     int verificaCastigator();
 };
-
-#endif
