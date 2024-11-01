67c228da8844872d3bdabefbd9caa82c8f94fbd6
Davidenco Nicolae <nagasakialbedo@gmail.com>
2024-11-01 23:18:41 +0300

    Update Joc.cpp

--- Joc.cpp
+++ Joc.cpp
@@ -1,44 +1,78 @@
-// Constructor implicit
-Joc::Joc() : marcaj_curent('X'), jucator_curent(1) {}
+#include "Joc.h"
 
-// Constructor cu parametri
 Joc::Joc(char marcaj_jucator1) : marcaj_curent(marcaj_jucator1), jucator_curent(1) {}
 
-// Constructor de copiere
 Joc::Joc(const Joc& other) {
-    this->tabla = other.tabla;
-    this->marcaj_curent = other.marcaj_curent;
-    this->jucator_curent = other.jucator_curent;
+    tabla = other.tabla;
+    marcaj_curent = other.marcaj_curent;
+    jucator_curent = other.jucator_curent;
 }
 
-// Operator de copiere
 Joc& Joc::operator=(const Joc& other) {
     if (this != &other) {
-        this->tabla = other.tabla;
-        this->marcaj_curent = other.marcaj_curent;
-        this->jucator_curent = other.jucator_curent;
+        tabla = other.tabla;
+        marcaj_curent = other.marcaj_curent;
+        jucator_curent = other.jucator_curent;
     }
     return *this;
 }
 
-// Operator de comparatie
 bool Joc::operator==(const Joc& other) const {
-    return (this->tabla == other.tabla &&
-            this->marcaj_curent == other.marcaj_curent &&
-            this->jucator_curent == other.jucator_curent);
-}
+    return (tabla == other.tabla && marcaj_curent == other.marcaj_curent && jucator_curent == other.jucator_curent);
+   }
 
-// Operator << pentru afisare
+
 ostream& operator<<(ostream& os, const Joc& joc) {
-    os << "Jucator curent: " << joc.jucator_curent << " cu marcajul " << joc.marcaj_curent;
+    os << "Jucător curent: " << joc.jucator_curent << " cu marcajul " << joc.marcaj_curent;
     return os;
-}
+ }
 
-// Operator >> pentru citire
 istream& operator>>(istream& is, Joc& joc) {
-    cout << "Introdu marcajul jucatorului: ";
+    cout << "Introduceți marcajul jucătorului: ";
     is >> joc.marcaj_curent;
-    cout << "Introdu jucatorul curent (1 sau 2): ";
+    cout << "Introduceți jucătorul curent (1 sau 2): ";
     is >> joc.jucator_curent;
     return is;
+   }
+
+void Joc::schimbaJucator() {
+    jucator_curent = (jucator_curent == 1) ? 2 : 1;
+    marcaj_curent = (marcaj_curent == 'X') ? 'O' : 'X';
+ }
+
+void Joc::incepeJoc() {
+    tabla.deseneazaTabla();
+    int castigator = 0;
+
+    for (int i = 0; i < 9; i++) {
+        cout << "Este rândul jucătorului " << jucator_curent << ". Introdu poziția: ";
+        int pozitie;
+        cin >> pozitie;
+
+        if (pozitie < 1 || pozitie > 9) {
+            cout << "Poziție invalidă! Încearcă din nou.\n";
+            i--;
+            continue;
+        }
+
+        if (!tabla.puneMarcaj(pozitie, marcaj_curent)) {
+            cout << "Poziție ocupată! Încearcă din nou.\n";
+            i--;
+            continue;
+        }
+
+        tabla.deseneazaTabla();
+
+        castigator = tabla.verificaCastigator();
+        if (castigator != 0) {
+            cout << "Jucătorul " << jucator_curent << " a câștigat!\n";
+            break;
+        }
+
+        schimbaJucator();
+    }
+
+    if (castigator == 0) {
+        cout << "Egalitate!\n";
+    }
 }
