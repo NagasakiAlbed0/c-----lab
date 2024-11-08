19751e162c38084b5d00a074316bf48844c7984d
Davidenco Nicolae <nagasakialbedo@gmail.com>
2024-11-08 16:31:55 +0300

    Tabla.h

--- Tabla.h
+++ Tabla.h
@@ -2,26 +2,17 @@
 #define TABLA_H
 
 #include <iostream>
-
-using namespace std;
+#include <vector>
+#include <string>
 
 class Tabla {
 public:
-    char tabla[3][3];
+    std::vector<std::string> tabla;
 
     Tabla();
-    Tabla(const Tabla& other); 
-
-    Tabla& operator=(const Tabla& other);
-
-bool operator==(const Tabla& other) const;
-
     void deseneazaTabla();
-    bool puneMarcaj(int pozitie, char marcaj);
+    bool puneMarcaj(int pozitie, const std::string& marcaj);
     int verificaCastigator();
-    };
+};
 
-ostream& operator<<(ostream& os, const Tabla& tabla);
-  istream& operator>>(istream& is, Tabla& tabla);
-
- #endif
+#endif
