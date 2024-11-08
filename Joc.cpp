36e7bdc34e66e1a62c7e2cebf79e5b8158c3f387
Davidenco Nicolae <nagasakialbedo@gmail.com>
2024-11-08 16:31:38 +0300

    Joc.cpp

--- Joc.cpp
+++ Joc.cpp
@@ -1,71 +1,40 @@
 #include "Joc.h"
+#include <iostream>
 
-Joc::Joc(char marcaj_jucator1) : marcaj_curent(marcaj_jucator1), jucator_curent(1) {}
-
-Joc::Joc(const Joc& other) {
-    tabla = other.tabla;
-    marcaj_curent = other.marcaj_curent;
-    jucator_curent = other.jucator_curent;
-}
-
-Joc& Joc::operator=(const Joc& other) {
-    if (this != &other) {
-        tabla = other.tabla;
-        marcaj_curent = other.marcaj_curent;
-        jucator_curent = other.jucator_curent;
-    }
-    return *this;
-}
-
-bool Joc::operator==(const Joc& other) const {
-    return (tabla == other.tabla && marcaj_curent == other.marcaj_curent && jucator_curent == other.jucator_curent);
-   }
-
-
-ostream& operator<<(ostream& os, const Joc& joc) {
-    os << "Jucător curent: " << joc.jucator_curent << " cu marcajul " << joc.marcaj_curent;
-    return os;
- }
-
-istream& operator>>(istream& is, Joc& joc) {
-    cout << "Introduceți marcajul jucătorului: ";
-    is >> joc.marcaj_curent;
-    cout << "Introduceți jucătorul curent (1 sau 2): ";
-    is >> joc.jucator_curent;
-    return is;
-   }
+Joc::Joc(std::string marcaj_jucator1) 
+    : tabla(std::make_unique<Tabla>()), marcaj_curent(marcaj_jucator1), jucator_curent(1) {}
 
 void Joc::schimbaJucator() {
     jucator_curent = (jucator_curent == 1) ? 2 : 1;
-    marcaj_curent = (marcaj_curent == 'X') ? 'O' : 'X';
- }
+    marcaj_curent = (marcaj_curent == "X") ? "O" : "X";
+}
 
 void Joc::incepeJoc() {
-    tabla.deseneazaTabla();
+    tabla->deseneazaTabla();
     int castigator = 0;
 
     for (int i = 0; i < 9; i++) {
-        cout << "Este rândul jucătorului " << jucator_curent << ". Introdu poziția: ";
+        std::cout << "Este rândul jucătorului " << jucator_curent << ". Introdu poziția: ";
         int pozitie;
-        cin >> pozitie;
+        std::cin >> pozitie;
 
         if (pozitie < 1 || pozitie > 9) {
-            cout << "Poziție invalidă! Încearcă din nou.\n";
+            std::cout << "Poziție invalidă! Încearcă din nou.\n";
             i--;
             continue;
         }
 
-        if (!tabla.puneMarcaj(pozitie, marcaj_curent)) {
-            cout << "Poziție ocupată! Încearcă din nou.\n";
+        if (!tabla->puneMarcaj(pozitie, marcaj_curent)) {
+            std::cout << "Poziție ocupată! Încearcă din nou.\n";
             i--;
             continue;
         }
 
-        tabla.deseneazaTabla();
+        tabla->deseneazaTabla();
 
-        castigator = tabla.verificaCastigator();
+        castigator = tabla->verificaCastigator();
         if (castigator != 0) {
-            cout << "Jucătorul " << jucator_curent << " a câștigat!\n";
+            std::cout << "Jucătorul " << jucator_curent << " a câștigat!\n";
             break;
         }
 
@@ -73,6 +42,6 @@ void Joc::incepeJoc() {
     }
 
     if (castigator == 0) {
-        cout << "Egalitate!\n";
+        std::cout << "Egalitate!\n";
     }
 }
