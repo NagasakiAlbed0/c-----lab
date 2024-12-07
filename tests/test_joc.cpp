ec1c8e07dbaea4e3d881257929a065b5f66fd8ae
Davidenco Nicolae <nagasakialbedo@gmail.com>
2024-12-07 23:21:50 +0300

    test_joc.cpp

--- tests/test_joc.cpp
+++ tests/test_joc.cpp
@@ -1,1 +1,19 @@
+#include "catch.hpp"
+#include "../Joc.h"
+
+TEST_CASE("Testare schimbare jucator", "[Joc]") {
+    Joc joc("X");
+    REQUIRE(joc.jucator_curent == 1);
+    REQUIRE(joc.marcaj_curent == "X");
+    joc.schimbaJucator();
+    REQUIRE(joc.jucator_curent == 2);
+    REQUIRE(joc.marcaj_curent == "O");
+}
+
+TEST_CASE("Testare initializare joc", "[Joc]") {
+    Joc joc("X");
+    REQUIRE(joc.tabla != nullptr);
+    REQUIRE(joc.marcaj_curent == "X");
+    REQUIRE(joc.jucator_curent == 1);
+}
 
