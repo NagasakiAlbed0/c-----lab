b5d8fd73eb4c52caab7f418579b7bf8c69eec223
Davidenco Nicolae <nagasakialbedo@gmail.com>
2024-12-07 23:09:46 +0300

    Update Joc.h

--- Joc.h
+++ Joc.h
@@ -5,15 +5,36 @@
 #include <memory>
 #include <string>
 
+/// <summary>
+/// Clasa Joc gestioneaza logica jocului Tic-Tac-Toe, incluzand schimbarea jucatorilor
+/// si initierea jocului.
+/// </summary>
 class Joc {
 public:
-    std::unique_ptr<Tabla> tabla;
-    std::string marcaj_curent;
-    int jucator_curent;
+    std::unique_ptr<Tabla> tabla;     ///< Tabla de joc.
+    std::string marcaj_curent;        ///< Marcajul jucatorului curent (X sau O).
+    int jucator_curent;               ///< Numarul jucatorului curent (1 sau 2).
 
+    /// <summary>
+    /// Constructor al clasei Joc.
+    /// </summary>
+    /// <param name="marcaj_jucator1">Marcajul cu care incepe jucatorul 1.</param>
     Joc(std::string marcaj_jucator1);
+
+    /// <summary>
+    /// Schimba jucatorul curent si actualizeaza marcajul.
+    /// </summary>
     void schimbaJucator();
+
+    /// <summary>
+    /// Initiaza si gestioneaza desfasurarea jocului.
+    /// </summary>
     void incepeJoc();
+
+    /// <summary>
+    /// Salveaza scorul in baza de date.
+    /// </summary>
+    /// <param name="castigator">Numele castigatorului sau "Egalitate".</param>
     void salveazaScor(const std::string& castigator);
 };
 
